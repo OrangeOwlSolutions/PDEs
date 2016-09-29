afacfe9c8cd00d36c28394c6d9d76b2a29e17196
Orange Owl <orangeowlsolutions@gmail.com>
2016-09-29 19:03:28 +0200

    Create leapFrog.m

---
+++ Advection-Equation/leapFrog.m
@@ -0,0 +1,47 @@
+% --- Solves the heat equation u_t + u_x = 0 in (0, 2 * pi) 
+
+% --- INPUTS
+% --- propagatingFunction           : Solution function
+% --- v                             : Wave speed
+% --- t0                            : Initial time
+% --- tf                            : Final time
+% --- M                             : Number of time steps
+% --- N                             : Number of space mesh points
+
+% --- OUTPUTS
+% --- u                             : Numerical solution
+% --- uRef                          : Exact solution
+% --- x                             : Space discretization points
+% --- t                             : Time discretization points
+function [u, uRef, x, t] = leapFrog(propagatingFunction, v, t_0, t_f, M, N)
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+% SPACE DISCRETIZATION %
+%%%%%%%%%%%%%%%%%%%%%%%%
+dx  = 2 * pi / N;                   % --- Discretization step in space
+x   = (0 : dx : 2 * pi)';           % --- Discretization points
+
+%%%%%%%%%%%%%%%%%%%%%%%
+% TIME DISCRETIZATION %
+%%%%%%%%%%%%%%%%%%%%%%%
+dt  = (t_f - t_0) / M;              % --- Discretization time
+t   = t_0 : dt : t_f;
+
+alpha = v * dt / dx;                % --- Courant number
+
+% --- Initialize the solutions (approximated and exact)
+u            = zeros(N + 1, M + 1); % --- u(u, t)
+u(:, 1)      = propagatingFunction(x - v * t(1));     % --- Initial condition
+u(:, 2)      = propagatingFunction(x - v * t(2));     % --- Initial condition
+uRef(:, 1)   = u(:, 1);
+uRef(:, 2)   = u(:, 2);
+
+for l = 2 : M - 1       % --- Time step
+
+    u(2 : N, l + 1)     = u(2 : N, l - 1) - (alpha) * ...
+                         (u(3 : N + 1, l) - u(1 : N - 1, l));
+    u(1, l + 1)         = propagatingFunction(x(1) - v * t(l + 1));
+%     u(N + 1, l + 1)     = 2 * u(N, l + 1) - u(N - 1, l + 1);
+    u(N + 1, l + 2)     = u(N + 1, l) - 2 * alpha * (u(N + 1, l + 1) - u(N, l + 1));
+    uRef(:, l + 1)      = propagatingFunction(x - v * t(l + 1));
+end
