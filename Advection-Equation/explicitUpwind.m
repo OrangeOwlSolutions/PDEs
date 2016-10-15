de10c13ad8e0a2d660c2ce06a65fe8ff5e167c93
Orange Owl <orangeowlsolutions@gmail.com>
2016-10-15 09:07:21 +0200

    Update explicitUpwind.m

--- Advection-Equation/explicitUpwind.m
+++ Advection-Equation/explicitUpwind.m
@@ -13,13 +13,13 @@
 % --- uRef                          : Exact solution
 % --- x                             : Space discretization points
 % --- t                             : Time discretization points
-function [u, uRef, x, t] = explicitDownwind(propagatingFunction, v, t_0, t_f, M, N)
+function [u, uRef, x, t] = explicitUpwind(propagatingFunction, v, t_0, t_f, M, N)
 
 %%%%%%%%%%%%%%%%%%%%%%%%
 % SPACE DISCRETIZATION %
 %%%%%%%%%%%%%%%%%%%%%%%%
 dx  = 2 * pi / N;                   % --- Discretization step in space
-x   = (0 : dx : 2 * pi)';           % --- Discretization points
+x   = (0 : dx : 2 * pi);           % --- Discretization points
 
 %%%%%%%%%%%%%%%%%%%%%%%
 % TIME DISCRETIZATION %
@@ -30,12 +30,15 @@ t   = t_0 : dt : t_f;
 alpha = v * dt / dx;                % --- Courant number
 
 % --- Initialize the solutions (approximated and exact)
-u            = zeros(N + 1, M + 1); % --- u(u, t)
-u(:, 1)      = propagatingFunction(x - v * t(1));     % --- Initial condition
-uRef(:, 1)   = u(:, 1);
+u            = zeros(M + 1, N + 1); % --- u(u, t); First row is for initial condition, first column is for boundary condition
+u(1, :)      = propagatingFunction(x - v * t(1));     % --- Initial condition
+uRef(1, :)   = u(1, :);
 
-for l = 1 : M       % --- Time step
-    u(2 : N + 1, l + 1) = u(2 : N + 1, l) - alpha * (u(2 : N + 1, l) - u(1 : N, l));
-    u(1, l + 1)         = propagatingFunction(x(1) - v * t(l + 1));
-    uRef(:, l + 1)      = propagatingFunction(x - v * t(l + 1));
+for l = 1 : M       % --- Time steps
+    u(l + 1, 2 : N + 1) = u(l, 2 : N + 1) - alpha * (u(l, 2 : N + 1) - u(l, 1 : N)); % --- Update equation
+    u(l + 1, 1)         = propagatingFunction(x(1) - v * t(l + 1)); % --- Enforcing boundary condition (left boundary)
+    % --- We are aware that we are not at all dealing with right boundary
+    % condition. The approach works due to the backward finite difference
+    % update.
+    uRef(l + 1, :)      = propagatingFunction(x - v * t(l + 1));
 end
