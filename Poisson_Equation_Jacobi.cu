06860f088882d53483ad5b63a8892cdd4ed69aee
Orange Owl <orangeowlsolutions@gmail.com>
2018-10-20 08:30:05 +0200

    Update Poisson_Equation_Jacobi.cu

--- Poisson_Equation_Jacobi.cu
+++ Poisson_Equation_Jacobi.cu
@@ -1,86 +1,68 @@
-
 #include <stdio.h>
 #include <string.h>
+#include <fstream>
+#include <iomanip>
 
-#include "Utilities.cuh"
+// --- Greek pi
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-#define BLOCK_SIZE 32
+#define BLOCKSIZEX 16
+#define BLOCKSIZEY 16
 
-/********************************/
-/* HOST INITIALIZATION FUNCTION */
-/********************************/
-void init(double * __restrict U, double * __restrict U_old, double * __restrict F, const double x_min, const double x_max, const double y_min, const double y_max, double delta, int Nb)
+#define prec_save 10
+
+//#define REAL_TYPE	float
+#define REAL_TYPE	double
+
+/*******************/
+/* iDivUp FUNCTION */
+/*******************/
+int iDivUp(int a, int b){ return ((a % b) != 0) ? (a / b + 1) : (a / b); }
+
+/********************/
+/* CUDA ERROR CHECK */
+/********************/
+// --- Credit to http://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true)
 {
-    // --- Initilize grid coordinates
-    double x = -1.0;
-    double y = -1.0;
-
-	for (int i = 0; i < Nb; i++) {
-		for (int j = 0; j < Nb; j++) {
-            
-			F    [i * (Nb) + j]	= 0.0;
-            U    [i * (Nb) + j]	= 0.0;
-            U_old[i * (Nb) + j] = 0.0;
-            
-			// --- Set radiator temperature in the source box
-            if (x <= x_max && x >= x_min && y <= y_max && y >= y_min) F[i * Nb + j] = 200.0;
-
-			// --- Boundary condition on the left, right and upper walls. The boundary condition on the lower wall is vanishing temperature
-            if (i == (Nb - 1) || i == 0 || j == (Nb - 1))
-            {
-                U    [i * (Nb) + j] = 20.0;
-                U_old[i * (Nb) + j] = 20.0;
-            }
-            // --- Update y-grid coordinate
-            y += delta;
-        }
-        // --- Update x-grid coordinate
-        x += delta;
-        y = -1.0;
-    }
+	if (code != cudaSuccess)
+	{
+		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+		if (abort) { exit(code); }
+	}
 }
 
-/*********************************/
-/* JACOBI ITERATOR HOST FUNCTION */
-/*********************************/
-void jacobi_iterator_CPU(const double * __restrict__ U, double * __restrict__ U_old, const double * __restrict__ F, const double delta2, const int Nb)
-{
-	for (int i=1; i<Nb-1; i++)
-		for (int j=1; j<Nb-1; j++)
-	        U_old[j * Nb + i] = (U[j * Nb + (i - 1)] + U[j * Nb + (i + 1)] + U[(j - 1) * Nb + i] + U[(j + 1) * Nb + i] + (delta2 * F[j * Nb + i])) * 0.25;
-
-}
+void gpuErrchk(cudaError_t ans) { gpuAssert((ans), __FILE__, __LINE__); }
 
 /***********************************/
 /* JACOBI ITERATOR KERNEL FUNCTION */
 /***********************************/
-__global__ void jacobi_iterator_GPU(const double * __restrict__ U, double * __restrict__ U_old, const double * __restrict__ F, const double delta2, const int Nb)
+template <class T>
+__global__ void jacobiIteratorGPU(const T * __restrict__ d_u, T * __restrict__ d_u_old, const T * __restrict__ d_f, const T dxdy, const int M, const int N)
 {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-    int j = blockDim.y * blockIdx.y + threadIdx.y;
-    
-	if (i < Nb - 1 && j < Nb - 1 && i > 0 && j > 0)
-    {
-        U_old[j * Nb + i] = (U[j * Nb + (i - 1)] + U[j * Nb + (i + 1)] + U[(j - 1) * Nb + i] + U[(j + 1) * Nb + i] + (delta2 * F[j * Nb + i])) * 0.25;
+	int i = blockDim.x * blockIdx.x + threadIdx.x;
+	int j = blockDim.y * blockIdx.y + threadIdx.y;
 
-    }
+	if (i < M - 1 && j < N - 1 && i > 0 && j > 0)
+		d_u_old[j * M + i] = (d_u[j * M + (i - 1)] + d_u[j * M + (i + 1)] + d_u[(j - 1) * M + i] + d_u[(j + 1) * M + i] + (dxdy * d_f[j * M + i])) * 0.25;
 }
 
-/***************************/
-/* DISPLAY MATRIX FUNCTION */
-/***************************/
-void print_matrix(int N, double *M)
-{
-    int Nb = N + 2;
-    int i, j;
-    for (i = Nb - 1; i >= 0; i--)
-    {
-        for (j = 0; j < Nb; j++)
-        {
-            printf("%.2f\t", M[j * Nb + i]);
-        }
-        printf("\n");
-    }
+/*************************************/
+/* SAVE FLOAT ARRAY FROM GPU TO FILE */
+/*************************************/
+template <class T>
+void saveGPUrealtxt(const T * d_in, const char *filename, const int M) {
+
+	T *h_in = (T *)malloc(M * sizeof(T));
+
+	gpuErrchk(cudaMemcpy(h_in, d_in, M * sizeof(T), cudaMemcpyDeviceToHost));
+
+	std::ofstream outfile;
+	outfile.open(filename);
+	for (int i = 0; i < M; i++) outfile << std::setprecision(prec_save) << h_in[i] << "\n";
+	outfile.close();
+
 }
 
 /********/
@@ -88,84 +70,80 @@ void print_matrix(int N, double *M)
 /********/
 int main()
 {
-	// --- The computation domain is [-1, 1] x [-1, 1]
-	
-	const int N			= 16;							// --- Grid size is N x N
-    const int Nb		= N + 2;						// --- Grid side including the boundaries is Nb x Nb
-    const int MAX_ITER	= 1000;							// --- Maximum number of iterations
+	const int		M			= 80;							// --- Number of discretization points along x
+	const int		N			= 100;							// --- Number of discretization points along y
+	const int		numIter		= 2000;							// --- Number of iterations
+	const REAL_TYPE dx			= 1.f / ((REAL_TYPE)M - 1.f);	// --- Discretization step along x
+	const REAL_TYPE	dy			= 1.f / ((REAL_TYPE)N - 1.f);	// --- Discretization step along y
 
-    // --- Defining the source box
-	double x_min = 0.0;
-    double x_max = 1.0 / 3.0;
-    double y_min = -2.0 / 3.0;
-    double y_max = -1.0 / 3.0;
+	/****************************************************/
+	/* DISCRETIZATION POINTS ALONG X ON HOST AND DEVICE */
+	/****************************************************/
+	REAL_TYPE *h_x = (REAL_TYPE *)malloc(M * sizeof(REAL_TYPE));
+	REAL_TYPE *h_y = (REAL_TYPE *)malloc(N * sizeof(REAL_TYPE));
+	for (int k = 0; k < M; k++) h_x[k] = k * dx;
+	for (int k = 0; k < N; k++) h_y[k] = k * dy;
 
-	double delta		= 2.0 / ((double)N - 1.0);		// --- Discretization step
+	int deviceCount;
+	gpuErrchk(cudaGetDeviceCount(&deviceCount));
 
-	// --- Allocating host memory variables
-    double *h_U				= (double *)malloc(Nb * Nb * sizeof(double));
-    double *h_U_old			= (double *)malloc(Nb * Nb * sizeof(double));
-    double *h_F				= (double *)malloc(Nb * Nb * sizeof(double));
+	REAL_TYPE *d_x; gpuErrchk(cudaMalloc(&d_x, M * sizeof(REAL_TYPE)));
+	REAL_TYPE *d_y; gpuErrchk(cudaMalloc(&d_y, N * sizeof(REAL_TYPE)));
+	gpuErrchk(cudaMemcpy(d_x, h_x, M * sizeof(REAL_TYPE), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_y, h_y, N * sizeof(REAL_TYPE), cudaMemcpyHostToDevice));
 
-	// --- Allocating device memory variables
-	double *d_U;			gpuErrchk(cudaMalloc(&d_U,		Nb * Nb * sizeof(double)));
-    double *d_U_old;		gpuErrchk(cudaMalloc(&d_U_old,	Nb * Nb * sizeof(double)));
-	double *d_F;			gpuErrchk(cudaMalloc(&d_F,		Nb * Nb * sizeof(double)));
+	/**********************************/
+	/* SOURCE TERM ON HOST AND DEVICE */
+	/**********************************/
+	REAL_TYPE *h_f = (REAL_TYPE *)malloc(M * N * sizeof(REAL_TYPE));
+	for (int n = 0; n < N; n++)
+		for (int m = 0; m < M; m++)
+			h_f[n * M + m] = h_x[m] * h_x[m] + h_y[n] * h_y[n];
 
-	// --- Dummy pointer for pointer swapping
-	double *temp;
+	REAL_TYPE *d_f; gpuErrchk(cudaMalloc(&d_f, M * N * sizeof(REAL_TYPE)));
+	gpuErrchk(cudaMemcpy(d_f, h_f, M * N * sizeof(REAL_TYPE), cudaMemcpyHostToDevice));
 
-    // --- Host array initialization
-    init(h_U, h_U_old, h_F, x_min, x_max, y_min, y_max, delta, Nb);
+	/*************************************************************************/
+	/* INITIALIZING THE SOLUTION WITH BOUNDARY CONDITIONS ON HOST AND DEVICE */
+	/*************************************************************************/
+	REAL_TYPE *h_u = (REAL_TYPE *)calloc(M * N, sizeof(REAL_TYPE));
+	REAL_TYPE *d_u;		gpuErrchk(cudaMalloc(&d_u, M * N * sizeof(REAL_TYPE)));
+	REAL_TYPE *d_u_old; gpuErrchk(cudaMalloc(&d_u_old, M * N * sizeof(REAL_TYPE)));
 
-    // --- Copying arrays from host to device
-    gpuErrchk(cudaMemcpy(d_U,		h_U,		Nb * Nb * sizeof(double), cudaMemcpyHostToDevice));
-    gpuErrchk(cudaMemcpy(d_U_old,	h_U_old,	Nb * Nb * sizeof(double), cudaMemcpyHostToDevice));
-    gpuErrchk(cudaMemcpy(d_F,		h_F,		Nb * Nb * sizeof(double), cudaMemcpyHostToDevice));
-    
-    // --- Host iterations
-    for (int h = 0; h < MAX_ITER; h++)
-    {
-        jacobi_iterator_CPU(h_U, h_U_old, h_F, delta * delta, Nb);
+	for (int m = 0; m < M; m++) {
+		h_u[(N - 1) * M + m] = h_x[m] * h_x[m] / 2.f;	// --- Upper boundary condition
+		h_u[0 * M + m]		 = 0.f;						// --- Lower boundary condition
+	}
+	for (int n = 0; n < N; n++) {
+		h_u[n * M + 0]		 = sin(M_PI * h_y[n]);						// --- Left boundary condition
+		h_u[n * M + M - 1]   = exp(M_PI) * sin(M_PI * h_y[n]) + h_y[n] * h_y[n] / 2.f;							
+																		// --- Right boundary condition
+	}
 
-		// --- Pointers swap
-        temp = h_U;
-        h_U = h_U_old;
-        h_U_old = temp;
-    }
-	
-	printf("Host results\n");
-	print_matrix(N, h_U);
+	gpuErrchk(cudaMemcpy(d_u,	  h_u, M * N * sizeof(REAL_TYPE), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_u_old, d_u, M * N * sizeof(REAL_TYPE), cudaMemcpyDeviceToDevice));
 
+	/**************/
+	/* ITERATIONS */
+	/**************/
 	// --- Device iterations
-	dim3 DimBlock(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 DimGrid(iDivUp(N, BLOCK_SIZE), iDivUp(N, BLOCK_SIZE));
+	dim3 DimBlock(BLOCKSIZEX, BLOCKSIZEY);
+	dim3 DimGrid(iDivUp(M, BLOCKSIZEX), iDivUp(N, BLOCKSIZEY));
 
-    for (int h = 0; h < MAX_ITER; h++)
-    {
-        jacobi_iterator_GPU<<<DimGrid, DimBlock>>>(d_U, d_U_old, d_F, delta * delta, Nb);
+	for (int k = 0; k < numIter; k++)
+	{
+		jacobiIteratorGPU << <DimGrid, DimBlock >> >(d_u, d_u_old, d_f, dx * dx, M, N);
+		gpuErrchk(cudaPeekAtLastError());
+		gpuErrchk(cudaDeviceSynchronize());
 
 		// --- Pointers swap
-        temp = d_U;
-        d_U = d_U_old;
-        d_U_old = temp;
-    }
+		REAL_TYPE *temp = d_u;
+		d_u = d_u_old;
+		d_u_old = temp;
+	}
 
-    // --- Move device result to the host
-	gpuErrchk(cudaMemcpy(h_U, d_U, Nb * Nb * sizeof(double), cudaMemcpyDeviceToHost));
-    
-	printf("Device results\n");
-	print_matrix(N, h_U);
+	// --- Save results from GPU to file
+	saveGPUrealtxt(d_u, ".\\d_result.txt", M * N);
 
-	// --- Freeing host memory
-    free(h_U);
-    free(h_U_old);
-    free(h_F);
-
-	// --- Freeing device memory
-    gpuErrchk(cudaFree(d_U));
-    gpuErrchk(cudaFree(d_U_old));
-    gpuErrchk(cudaFree(d_F));
-    
 	return 0;
 }
